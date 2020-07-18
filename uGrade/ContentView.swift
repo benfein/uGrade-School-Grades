d09bb0ad78cc509889f311b36e2459c54172d29f
Benjamin Fein <benfein78@icloud.com>
2020-07-18 14:31:42 -0400

    Initial Commit

---
+++ uGrade/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uGrade
+//
+//  Created by Ben Fein on 7/18/20.
+//  Copyright © 2020 Ben Fein. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
